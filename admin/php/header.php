0954ad8e95a63a20b8f1c8a74f163bcac76979cc
arsshe14 <arsshe14@htl-shkoder.com>
2019-09-27 10:08:52 +0200

    PHP statt HTML

--- admin/php/header.php
+++ admin/php/header.php
@@ -1,32 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
 	<title>Home</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="description" content="">
-    <meta name="author" content="Infotainment">
-    <meta name="generator" content="v1.0.0">
+	<meta name="description" content="">
+	<meta name="author" content="Infotainment">
+	<meta name="generator" content="v1.0.0">
 
-    <!-- Mobile viewport Optimierung -->
+  	<!-- Mobile viewport Optimierung -->
     
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon"/>
 
-  	<!-- Bootstrap CSS File-->
-  	<link rel="stylesheet" href="../css/bootstrap.min.css">
+ 	 <!-- Bootstrap CSS File-->
+  	<link rel="stylesheet" href="css/bootstrap.min.css">
 
-	<script src="../js/jquery.min.js"></script>
+	
+	<script src="js/jquery.min.js"></script>
 	  
   	<!-- Bootstrap JS File-->
-	<script src="../js/jquery-3.3.1.slim.min.js"></script>
-    <script src="../js/popper.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+	<script src="js/jquery-3.3.1.slim.min.js"></script>
+  	<script src="js/popper.min.js"></script>
+  	<script src="js/bootstrap.min.js"></script>
     
    
-    <script src="../js/script.js"></script>
+  	<script src="js/script.js"></script>
+  	<script src="js/fontawesome.js"></script>
 
-    <!-- CSS File-->
-    <link rel="stylesheet" href="../css/main.css">
+	<!-- Custom styles for this template-->
+	<link href="css/admin.min.css" rel="stylesheet">
+
+	<!-- CSS File-->
+	<link rel="stylesheet" href="css/main.css">
 	
 	<style>
 	button a {
@@ -45,8 +50,6 @@
     }
 	
 	</style>
-
-
 
 </head>
 
--- admin/php/navigation.php
+++ admin/php/navigation.php
@@ -1,29 +1,106 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
+<!--<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">-->
+    
+<nav class="navbar navbar-expand-lg fixed-top flex-md-nowrap navbar-dark bg-dark p-0 shadow">
+    <a class="navbar-logo" href="#">
+      <img src="img/logo.png" alt="Foto">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <ul class="navbar-nav px-3">
+      <li class="nav-item text-nowrap">
+        <a class="nav-link" href="#">
+          Admin
+          <span class="favicon">
+            <i class="fas fa-user"></i>
+          </span>
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
       </li>
     </ul>
+  </nav>
+
+  <div class="container-fluid">
+    <div class="row">
+      <nav class="navcol-sm-2 d-none d-sm-block bg-dark sidebar">
+        <div class="sidebar-sticky">
+          <ul class="nav flex-column mr-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">
+              <span class="favicon">
+                <i class="fas fa-home"></i>
+              </span>
+                Dashboard <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+              <span class="favicon">
+                <i class="fas fa-home"></i>
+              </span>
+                Orders
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="shopping-cart"></span>
+                Products
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="users"></span>
+                Customers
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="bar-chart-2"></span>
+                Reports
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="layers"></span>
+                Integrations
+              </a>
+            </li>
+          </ul>
+
+          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Saved reports</span>
+            <a class="d-flex align-items-center text-muted" href="#">
+              <span data-feather="plus-circle"></span>
+            </a>
+          </h6>
+          <ul class="nav flex-column mb-2">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Current month
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Last quarter
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Social engagement
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Year-end sale
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </div>
-</nav>
