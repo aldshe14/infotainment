a0ac723f05d95aae7fc93b6283363c89eaf286ff
Aldo Sheldija <aldshe14@htl-shkoder.com>
2019-11-03 01:03:36 +0100

    Supplierplan - Day choose option added

--- admin/supplierplan.php
+++ admin/supplierplan.php
@@ -10,6 +10,11 @@
     $rawDate = date("m/d/Y");
 
     $day = date('N', strtotime($rawDate));
+    if($day>5)
+        $day=1;
+
+    if(isset($_GET['day']))
+        $day = $_GET['day'];
 
     $sql = "SELECT * FROM tb_infotainment_unterricht where tag = :test and fach <> 'SU' group by lehrer order by klasse asc ;";
     $stmt = $con->prepare($sql);
@@ -24,7 +29,21 @@
 ?>
 
 <div class="container">
-    <h1 class="mt-4">Supplierplan</h1>
+    <div class="row">
+        <div class="col-md-4 mt-4">
+        <h1>Supplierplan</h1>
+        </div>
+        <div class="form-group col-md-4 mt-4">
+            <select name="day" class="form-control">
+                <option value="1" <?php if(isset($_GET['day'])){if($_GET['day']==1){ echo 'selected'; $selected = 1;}}else{ echo 'selected';$selected = 1;}?>>Montag</option>
+                <option value="2" <?php if(isset($_GET['day'])){if($_GET['day']==2){ echo 'selected'; $selected = 2;}}?>>Dienstag</option>
+                <option value="3" <?php if(isset($_GET['day'])){if($_GET['day']==3){ echo 'selected'; $selected = 3;}}?>>Mittwoch</option>
+                <option value="4" <?php if(isset($_GET['day'])){if($_GET['day']==4){ echo 'selected'; $selected = 4;}}?>>Donnerstag</option>
+                <option value="5" <?php if(isset($_GET['day'])){if($_GET['day']==5){ echo 'selected'; $selected =5;}}?>>Freitag</option>
+            </select>
+            
+        </div>
+    </div>
     <br>
     <div class="table-responsive">
         <table id="resetuserdata" class="table table-striped table-bordered" data-order='[[ 0, "asc" ]]' data-page-length='50'>
@@ -74,7 +93,30 @@
         </table>
     </div>
 
+<script type="text/javascript">
+        (function () {
+            var sel;
 
+            $("select[name=day]").focus(function () {
+                // Store the current value on focus, before it changes
+                sel = this.value;
+            }).change(function() {
+                // Do soomething with the previous value after the change
+                sel = this.value;
+                window.location.href = '?day='+sel;
+
+            });
+        })();
+        
+        $(document).ready(function(){
+        $(".test").click(function(){
+                $(this).next().show();
+                $(this).next().hide();
+            });
+
+        });
+        
+    </script>
 
 <?php
 	require_once "footer.php";
