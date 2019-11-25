aac9f8304b5eabd1cbbd511f52eee72dd0b13f27
Aldo Sheldija <aldshe14@htl-shkoder.com>
2019-11-26 00:39:29 +0100

    GetWebsitePost Added

--- Datenbank/website.sql
+++ Datenbank/website.sql
@@ -2,6 +2,8 @@ create table tb_infotainment_website_posts(
 	w_id int primary key auto_increment,
     title varchar(200) not null,
     content text not null,
-    image text not null,
+    image blob not null,
     datum datetime not null
 );
+
+drop table tb_infotainment_website_posts;
---
+++ scripts/getWebsitePost.php
@@ -0,0 +1,53 @@
+<?php
+	require_once "connection.php";
+
+    $sql = "SELECT * 
+    FROM  tb_infotainment_website_post 
+    order by datum desc limit 1 ;";
+    $pdo = $con->prepare($sql);
+
+    $title = "";
+
+    try {
+        $pdo->execute();
+        $post = $pdo->fetchAll(PDO::FETCH_ASSOC);
+        //echo $result['city_id'];
+        foreach ($post as $row){
+            $title = $row['title'];
+        }
+    }catch (PDOException $e) {
+    	header("Location: getWebsitePost.php");
+    }
+
+    $url="http://htl-shkoder.com/wp-json/wp/v2/posts";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+    $result=curl_exec($ch);
+    $posts = json_decode($result, true);
+    foreach ($posts as $post) { 
+        if($post['title']['rendered'] != $title){
+
+            $sql = "INSERT INTO `tb_infotainment_website_posts`(`title`, `content`, `image`, `datum`) 
+            VALUES(:title, :content, :image, :datum)";
+
+            $sth = $con->prepare($sql);
+            $blob = file_get_contents($post['jetpack_featured_media_url']);
+            $sth->bindValue(':title', $post['title']['rendered']);
+            $sth->bindValue(':content', $post['excerpt']['rendered']);
+            $sth->bindParam(':image', $blob,PDO::PARAM_LOB);
+            $sth->bindValue(':datum', $post['date']);
+
+            try {
+                $sth->execute();
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }else{
+            exit();
+        }
+    }
+    
+
+?>
