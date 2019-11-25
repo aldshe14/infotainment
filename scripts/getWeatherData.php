672ce9b14f6fab6c9b0283824b9f049aa10d1616
AldShe14 <aldshe14@htl-shkoder.com>
2019-11-26 00:39:49 +0100

    GetWeatherData - Updated

--- scripts/getWeatherData.php
+++ scripts/getWeatherData.php
@@ -34,19 +34,19 @@
 
     curl_close($ch);
     $data = json_decode($response);
-    $currentTime = time();
+    $currentTime = date("Y-m-d H:i:s");
 
     $sql = "INSERT INTO `tb_infotainment_weather` (`datum`, `beschreibung`, `temp`, `temp_min`, `temp_max`, `humidity`, `sunrise`, `sunset`, `wind_speed`, `icon`, `city_name`) 
     VALUES(:datum, :beschreibung, :temp, :min, :max, :humidity, :sunrise, :sunset, :windspeed, :icon, :city_name)";
     $sth = $con->prepare($sql);
-    $sth->bindValue(':datum', date("Y-m-d H:m:s", $currentTime));
+    $sth->bindValue(':datum', $currentTime);
     $sth->bindValue(':beschreibung', ucwords($data->weather[0]->description));
     $sth->bindValue(':temp', $data->main->temp);
     $sth->bindValue(':min', $data->main->temp_min);
     $sth->bindValue(':max', $data->main->temp_max);
     $sth->bindValue(':humidity', $data->main->humidity);
-    $sth->bindValue(':sunrise', date('Y-m-d H:i:s',strtotime('+1 hour',strtotime(gmdate("Y-m-d\TH:i:s\Z", $data->sys->sunrise)))));
-    $sth->bindValue(':sunset', date('Y-m-d H:i:s',strtotime('+1 hour',strtotime(gmdate("Y-m-d\TH:i:s\Z", $data->sys->sunset)))));
+    $sth->bindValue(':sunrise', date('Y-m-d H:i:s',strtotime('+0 hour',strtotime(gmdate("Y-m-d\TH:i:s\Z", $data->sys->sunrise)))));
+    $sth->bindValue(':sunset', date('Y-m-d H:i:s',strtotime('+0 hour',strtotime(gmdate("Y-m-d\TH:i:s\Z", $data->sys->sunset)))));
     $sth->bindValue(':windspeed', $data->wind->speed);
     $sth->bindValue(':icon', $data->weather[0]->icon);
     $sth->bindValue(':city_name', $data->name);
