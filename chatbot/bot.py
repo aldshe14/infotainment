798bb0085e6087411e03a334d9641e72d76af3d2
AldShe14 <aldshe14@htl-shkoder.com>
2019-11-27 00:12:11 +0100

    Chabot V1.1

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -4,6 +4,7 @@ import sys
 import time
 import telepot
 from telepot.loop import MessageLoop
+import wget
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -14,9 +15,18 @@ def handle(msg):
 
     if content_type == 'photo':
     	bot.sendMessage(chat_id, "Image received")
-
-bot = telepot.Bot('960505913:AAG5EqEksnoiXLZj0C-VmZ9UdbvjuSvDg1g')
-
+        
+        print (msg)
+        fileid = msg['photo'][2]['file_id']
+    	bot.sendPhoto(chat_id,fileid)
+        print(fileid)
+        file = bot.getFile(fileid)
+        filepath = file['file_path']
+        print (filepath)
+        url = "https://api.telegram.org/file/bot%s/%s" % (token,filepath)
+        wget.download(url, '/home/pi/chatbot/test.jpg')
+token = '960505913:AAG5EqEksnoiXLZj0C-VmZ9UdbvjuSvDg1g'
+bot = telepot.Bot(token)
 MessageLoop(bot, handle).run_as_thread()
 
 
