c0446020cfbae77604e823285b491a6307652b44
Aldo Sheldija <aldshe14@htl-shkoder.com>
2019-11-26 01:13:26 +0100

    GetWebsitePost - Working !!

--- scripts/getWebsitePost.php
+++ scripts/getWebsitePost.php
@@ -2,7 +2,7 @@
 	require_once "connection.php";
 
     $sql = "SELECT * 
-    FROM  tb_infotainment_website_post 
+    FROM  tb_infotainment_website_posts 
     order by datum desc limit 1 ;";
     $pdo = $con->prepare($sql);
 
@@ -16,8 +16,9 @@
             $title = $row['title'];
         }
     }catch (PDOException $e) {
-    	header("Location: getWebsitePost.php");
+    	//header("Location: getWebsitePost.php");
     }
+   
 
     $url="http://htl-shkoder.com/wp-json/wp/v2/posts";
     $ch = curl_init();
@@ -28,24 +29,27 @@
     $posts = json_decode($result, true);
     foreach ($posts as $post) { 
         if($post['title']['rendered'] != $title){
-
+            
             $sql = "INSERT INTO `tb_infotainment_website_posts`(`title`, `content`, `image`, `datum`) 
             VALUES(:title, :content, :image, :datum)";
 
             $sth = $con->prepare($sql);
+
             $blob = file_get_contents($post['jetpack_featured_media_url']);
+
             $sth->bindValue(':title', $post['title']['rendered']);
             $sth->bindValue(':content', $post['excerpt']['rendered']);
-            $sth->bindParam(':image', $blob,PDO::PARAM_LOB);
+            $sth->bindParam(':image', $blob, PDO::PARAM_LOB);
             $sth->bindValue(':datum', $post['date']);
 
             try {
                 $sth->execute();
             } catch (PDOException $e) {
                 echo $e->getMessage();
+                break;
             }
         }else{
-            exit();
+            break;
         }
     }
     
