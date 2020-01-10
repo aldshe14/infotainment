af4e4889a88c5c7967a690eebe32a1bbb99ab9fa
Aldo Sheldija <aldshe14@htl-shkoder.com>
2020-01-10 10:29:11 +0100

    AdminPanel: Layout add updated

--- admin/addLayout.php
+++ admin/addLayout.php
@@ -11,23 +11,22 @@
  
         // Validate name
             // Prepare an insert statement
-            $sql = "UPDATE  tb_infotainment_layout SET name=:name, beschreibung=:beschreibung, file=:file where l_id=:id";
-            
+            $sql = "Insert into tb_infotainment_layout(name,beschreibung,file) VALUES (:name,:beschreibung,:file)";
+             
             if($sth = $con->prepare($sql)){
                 // Bind variables to the prepared statement as parameters
                 $sth->bindParam(':name', $_POST["name"]);
                 $sth->bindParam(':beschreibung', $_POST["beschreibung"]);
                 $sth->bindParam(':file', $_POST["file"]);
-                $sth->bindParam(':id', $_GET["id"]);
                 try {
                     $sth->execute();
                     //header('Location: users.php?insert=done');
                     echo "<div id='hide' class=\"alert alert-success \">";
-                    echo "<p>Layout u ndryshua me sukses!</p>";
+                    echo "<p>Layout u shtua me sukses!</p>";
                     echo "</div>";
                     echo "<script> setTimeout(function(){
                         window.location.href = 'layouts.php';
-                     }, 2000);</script>";
+                     }, 4000);</script>";
 
                 } catch (PDOException $e) {
                     echo "<div id='hide' class=\"alert alert-danger \">";
@@ -39,45 +38,32 @@
             }
         
     }
-   
-    $sql = "SELECT * FROM tb_infotainment_layout WHERE l_id =" . $_GET["id"].";";
-    $sth = $con->prepare($sql);
-    $sth->execute();
-    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
 <div class="container">
-    <h1 class="mt-4">Edit Layout</h1>
+        <h1 class="mt-4">Shto Layout</h1>
         <br>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$_GET['id']; ?>" method="post">
-        <div class="form-row">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group col-sm-3">
                 <label>Name</label>
-                <input type="text" name="name" class="form-control" value="<?php echo $result[0]['name']?>" required>
+                <input type="text" name="name" class="form-control" required>
             </div>
-        </div>
-        <div class="form-row">
             <div class="form-group col-sm-3">
-                <label>Beschreibung</label>
-                <input type="text" name="beschreibung" class="form-control" value="<?php echo $result[0]['beschreibung']?>" required>
+            <label>Beschreibung</label>
+                <input type="text" name="beschreibung" class="form-control" required>
             </div>
-        </div>
-        <div class="form-row">
             <div class="form-group col-sm-3">
                 <label>File</label>
-                <input type="text" name="file" class="form-control" value="<?php echo $result[0]['file']?>" required>
+                <input type="text" name="file" class="form-control" required>
             </div>
-        </div>
             <br>
-        
-            <br>
-            <div class="form-group">
-                <button type="submit" class="btn btn-dark btn-lg" value="Submit">Edit</button>
+            <div class="form-group col-sm-3">
+                <button type="submit" class="btn btn-dark btn-lg" value="Submit">Shto</button>
             </div>
         </form>
-	</div>
+    </div>
 
 <?php
-	require_once "footer.php";
+    require_once "footer.php";
 ?>
