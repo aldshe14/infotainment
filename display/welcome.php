8f3b27a911c32e84e451aab23851f4517c2f56ec
Aldo Sheldija <aldshe14@htl-shkoder.com>
2020-01-31 10:12:57 +0100

    Display: Welcome updated insert added IP

--- display/welcome.php
+++ display/welcome.php
@@ -8,13 +8,13 @@
         //$mac = explode("\t", $lines[1]);
 		$mac = explode(" ", $lines[0]);
 		print_r($mac);
-		return $mac[1];
+		return $mac[5];
 		
 	}
 	
 	function getIPAddress(){
         $ip = false;
-        $arp = `ip address | grep "inet "`;
+        $arp = `ip address | grep inet`;
         $lines = explode("\n", $arp);
         //$mac = explode("\t", $lines[1]);
 		$ip = explode(" ", $lines[1]);
