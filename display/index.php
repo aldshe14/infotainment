6bd7869915d7e4547f0dbac612254cd90d3a96c0
Aldo Sheldija <aldshe14@htl-shkoder.com>
2020-02-01 13:36:12 +0100

    Display: Welcome updated insert added IP

--- display/index.php
+++ display/index.php
@@ -2,6 +2,36 @@
     require_once "php/connection.php";
     require_once "php/functions.php";
 
+    //$MAC = getMac();
+    $MAC = "b8:27:eb:c1:e6:4e";
+    $sql = "SELECT d_id,file
+    FROM tb_infotainment_display d
+    join tb_infotainment_layout l
+    on d.layout_id = l.l_id
+    where mac = :mac and l.name not like '-';
+    ";
+    $stmt = $con->prepare($sql);
+    $stmt->bindValue(":mac",$MAC);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if(!$result){
+        !header('location:welcome.php');
+    }
+
+
+    $sql = "SELECT l.file as file
+            FROM tb_infotainment_display d
+            JOIN tb_infotainment_layout l
+            ON d.layout_id = l.l_id
+            where d.mac = :mac;
+            ";
+    $stmt = $con->prepare($sql);
+    $stmt->bindValue(":mac",$MAC);
+    $stmt->execute();
+    $result = $stmt->fetch();
+
+    $layout = $result['file'];
     //Only for test
     $layout = "layout1";
     
