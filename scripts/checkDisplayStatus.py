b611b68352780ee0cbe53fe395b47ced89d401eb
Aldo Sheldija <aldshe14@htl-shkoder.com>
2020-02-11 01:28:37 +0100

    Scripts: CheckDisplayStatus changed

--- scripts/checkDisplayStatus.py
+++ scripts/checkDisplayStatus.py
@@ -5,7 +5,7 @@ import subprocess
 import os
 
 with open(os.devnull, "wb") as limbo:
-        for n in xrange(1 10):
+        for n in xrange(1,10):
                 ip="10.2.7.{0}".format(n)
                 result=subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
                         stdout=limbo, stderr=limbo).wait()
