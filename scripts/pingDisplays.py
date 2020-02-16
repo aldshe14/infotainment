8d3e6351525eca02388bddfdcb7d3dc4dfe5ae09
Aldo Sheldija <aldshe14@htl-shkoder.com>
2020-02-16 21:41:07 +0100

    AdminPanel: ViewLayout Modal updated

--- scripts/pingDisplays.py
+++ scripts/pingDisplays.py
@@ -9,24 +9,21 @@ query=("Select d_id,ip from tb_infotainment_display")
 curs.execute(query)
 displays=curs.fetchall()
 
-for displays in display:
+for display in displays:
     response=os.system("ping -c 1 " + display[1])
     status=0
     if (response == 0):
         status = 1
 
-    query=("Select d_id,ip from tb_infotainment_display d join tb_infotainment_display_status ds on d.d_id = ds.d_id where d_id = %s ")
-    param=(display[0])
-    curs.execute(query,param)
+    query=("Select d_id,ip from tb_infotainment_display d join tb_infotainment_display_status ds on d.d_id = ds.d_id where d_id = %s ") %(display[0])
+    curs.execute(query)
     result=curs.fetchall()
 
     if (result):
-        update=("UPDATE tb_infotainment_display_status SET status=%s where d_id=%s")
-        param=(status,display[0])
-        curs.execute(update,param)
+        update=("UPDATE tb_infotainment_display_status SET status=%s where d_id=%s") % (status,display[0])
+        curs.execute(update)
         conn.commit()
     else:
-        insert=("INSERT INTO tb_infotainment_display_status(d_id,status,lastChecked) VALUES(%s,%s,now())")
-        param=(display[0],status)
-        curs.execute(insert,param)
+        insert=("INSERT INTO tb_infotainment_display_status(d_id,status,lastChecked) VALUES(%s,%s,now())") % (display[0],status)
+        curs.execute(insert)
         conn.commit()
