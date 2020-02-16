9bcb25568c5b03d03c977eeca87319d593c01a3b
Aldo Sheldija <aldshe14@htl-shkoder.com>
2020-02-16 21:37:54 +0100

    Scripts: Ping Display Insert or Update Database

--- scripts/pingDisplays.py
+++ scripts/pingDisplays.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 import os
+import MySQLdb
 
-lines=('10.2.7.10','10.2.7.1')
+conn=MySQLdb.connect('localhost','infotainment', '1nf0tainment', 'infotainment_system')
+curs=conn.cursor()
 
-for line in lines:
-    response=os.system("ping -c 1 " + line)
+query=("Select d_id,ip from tb_infotainment_display")
+curs.execute(query)
+displays=curs.fetchall()
+
+for displays in display:
+    response=os.system("ping -c 1 " + display[1])
+    status=0
     if (response == 0):
-        status = line.rstrip() + " is Reachable"
+        status = 1
+
+    query=("Select d_id,ip from tb_infotainment_display d join tb_infotainment_display_status ds on d.d_id = ds.d_id where d_id = %s ")
+    param=(display[0])
+    curs.execute(query,param)
+    result=curs.fetchall()
+
+    if (result):
+        update=("UPDATE tb_infotainment_display_status SET status=%s where d_id=%s")
+        param=(status,display[0])
+        curs.execute(update,param)
+        conn.commit()
     else:
-        status = line + " is Not reachable"
-    print(status)
+        insert=("INSERT INTO tb_infotainment_display_status(d_id,status,lastChecked) VALUES(%s,%s,now())")
+        param=(display[0],status)
+        curs.execute(insert,param)
+        conn.commit()
